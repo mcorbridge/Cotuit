fa97998e3ef8ae58d43f3c5f63b7f19087d15e3d
m_cor <mikecorbridge@gmail.com>
2021-07-16 16:34:46 -0400

    add some new animation examples: easing, tween keyframe

--- app/src/main/java/com/example/cotuit/test/PlayingWithAnimationSpec.kt
+++ app/src/main/java/com/example/cotuit/test/PlayingWithAnimationSpec.kt
@@ -1,8 +1,6 @@
 package com.example.cotuit.test
 
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
@@ -37,25 +35,33 @@ class PlayingWithAnimationSpec {
         fun DoPlayingWithAnimationSpec(navController: NavHostController) {
 
             var springBoxWidth by remember { mutableStateOf(0f) }
+            var tweenBoxWidth by remember { mutableStateOf(0f) }
             var keyframeBoxWidth by remember { mutableStateOf(0f) }
             var snapBoxWidth by remember { mutableStateOf(0f) }
-            var springBoxColor by remember { mutableStateOf(0xFF009688) } // alt 0xFFFF5722
+            var springColor by remember { mutableStateOf(0xFF009688) } // alt 0xFFFF5722
+            var tweenColor by remember { mutableStateOf(0xFF009688) }
             var keyframeColor by remember { mutableStateOf(0xFF009688) }
             var snapColor by remember { mutableStateOf(0xFF009688) }
 
             //Inner function
             fun viewController(view: String) {
                 springBoxWidth = 0f
+                tweenBoxWidth = 0f
                 keyframeBoxWidth = 0f
                 snapBoxWidth = 0f
-                springBoxColor = 0xFF009688
+                springColor = 0xFF009688
+                tweenColor = 0xFF009688
                 keyframeColor = 0xFF009688
                 snapColor = 0xFF009688
                 when (view) {
 
                     "spring" -> {
                         springBoxWidth = 1f
-                        springBoxColor = 0xFFFF5722
+                        springColor = 0xFFFF5722
+                    }
+                    "tween" -> {
+                        tweenBoxWidth = 1f
+                        tweenColor = 0xFFFF5722
                     }
                     "keyframe" -> {
                         keyframeBoxWidth = 1f
@@ -79,29 +85,43 @@ class PlayingWithAnimationSpec {
                     // allow navigation back to Main Menu
                     NavIcon.MenuIcon(navController = navController)
 
-                    Row {
+                    Row(verticalAlignment = Alignment.Bottom) {
                         Text(
                             "Spring, ",
                             fontWeight = FontWeight.ExtraBold,
-                            fontSize = 32.sp,
-                            color = Color(springBoxColor),
+                            fontSize = 22.sp,
+                            color = Color(springColor),
                             modifier = Modifier.clickable {
                                 viewController("spring")
                             }
                         )
                         Text(
+                            "Tween, ",
+                            fontWeight = FontWeight.ExtraBold,
+                            fontSize = 22.sp,
+                            color = Color(tweenColor),
+                            modifier = Modifier.clickable {
+                                viewController("tween")
+                            }
+                        )
+                        Text(
                             "Keyframes, ",
                             fontWeight = FontWeight.ExtraBold,
-                            fontSize = 32.sp,
+                            fontSize = 22.sp,
                             color = Color(keyframeColor),
                             modifier = Modifier.clickable {
                                 viewController("keyframe")
                             }
                         )
                         Text(
-                            "and Snap",
+                            "and ", fontWeight = FontWeight.ExtraBold,
+                            fontSize = 18.sp,
+                            color = Color(0xFF009688),
+                        )
+                        Text(
+                            " Snap",
                             fontWeight = FontWeight.ExtraBold,
-                            fontSize = 32.sp,
+                            fontSize = 22.sp,
                             color = Color(snapColor),
                             modifier = Modifier.clickable {
                                 viewController("snap")
@@ -110,8 +130,13 @@ class PlayingWithAnimationSpec {
                     }
 
                     Box(Modifier.fillMaxWidth()) {
+
                         Box(modifier = Modifier.fillMaxWidth(springBoxWidth)) {
                             Spring()
+                        }
+
+                        Box(modifier = Modifier.fillMaxWidth(tweenBoxWidth)) {
+                            Tween()
                         }
 
                         Box(modifier = Modifier.fillMaxWidth(keyframeBoxWidth)) {
@@ -121,6 +146,7 @@ class PlayingWithAnimationSpec {
                         Box(modifier = Modifier.fillMaxWidth(snapBoxWidth)) {
                             Snap()
                         }
+
                     }
                 }
             }
@@ -263,8 +289,307 @@ class PlayingWithAnimationSpec {
 }
 
 @Composable
+fun Tween() {
+
+    var arrowPosition by remember { mutableStateOf(ArrowPosition.INIT) }
+
+    val offsetAnimation0: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+        tween(1000, easing = FastOutSlowInEasing)
+    )
+
+    val offsetAnimation1: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+        tween(1000, easing = LinearEasing)
+    )
+
+    val offsetAnimation2: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+        tween(1000, easing = FastOutLinearInEasing)
+    )
+
+    val offsetAnimation3: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+        tween(1000, easing = LinearOutSlowInEasing)
+    )
+
+    val offsetAnimation4: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+        tween(1000, easing = CubicBezierEasing(0.4f, 0.0f, 0.8f, 0.8f))
+    )
+
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy((-10).dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation0)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text("tween(1000, FastOutSlowInEasing)")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation1)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text("tween(1000, LinearEasing)")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation2)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text("tween(1000, FastOutLinearInEasing)")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation3)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text("tween(1000, LinearOutSlowInEasing)")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation4)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text("tween(1000, Custom Bezier)")
+        }
+    }
+}
+
+@Composable
 fun KeyFrame() {
-    Text("keyframe TODO")
+
+    var arrowPosition by remember { mutableStateOf(ArrowPosition.INIT) }
+
+    val offsetAnimation0: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+
+        keyframes {
+            durationMillis = 1000
+            100.dp.at(500)
+        })
+
+    val offsetAnimation1: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+        keyframes {
+            durationMillis = 3000
+            50.dp.at(20).with(LinearEasing)
+            200.dp.at(600).with(LinearOutSlowInEasing)
+            250.dp.at(700).with(FastOutSlowInEasing)
+        })
+
+    val offsetAnimation2: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+        repeatable(iterations = 5, animation = tween(5000), repeatMode = RepeatMode.Restart)
+    )
+
+    val offsetAnimation3: Dp by animateDpAsState(
+        if (arrowPosition == ArrowPosition.INIT) 0.dp else 360.dp,
+        repeatable(iterations = 5, animation = tween(5000), repeatMode = RepeatMode.Reverse)
+    )
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy((-10).dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation0)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text("1000ms total, 100dp at 500ms")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation1)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text("total 3000ms 50dp at 20ms (LinearEasing) 200dp at 600ms (LinearOutSlowInEasing) 300dp at 700ms (FastOutSlowInEasing")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation2)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(" repeatable(iterations = 5, animation = tween(5000), repeatMode = RepeatMode.Restart)")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.circle_arrow),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(90.dp)
+                    .absoluteOffset(x = offsetAnimation3)
+                    .clickable {
+                        when (arrowPosition) {
+                            ArrowPosition.INIT -> {
+                                arrowPosition = ArrowPosition.START
+                            }
+                            ArrowPosition.START -> {
+                                arrowPosition = ArrowPosition.INIT
+                            }
+                        }
+                    }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(" repeatable(iterations = 5, animation = tween(5000), repeatMode = RepeatMode.Reverse)")
+        }
+    }
 }
 
 @Composable
@@ -274,5 +599,9 @@ fun Snap() {
 
 
 enum class TestBoxPosition {
+    INIT, START
+}
+
+enum class ArrowPosition {
     INIT, START
 }
