6daf17f14af81733e2140d47fda3b4c6bd53ece9
m_cor <mikecorbridge@gmail.com>
2021-07-22 10:29:32 -0400

    added some new examples

--- app/src/main/java/com/example/cotuit/slides/Slide3.kt
+++ app/src/main/java/com/example/cotuit/slides/Slide3.kt
@@ -4,14 +4,13 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.cotuit.test.MyDataClass
@@ -25,6 +24,12 @@ class Slide3 {
         fun Slide(slideState: SlideState, dataClass: MyDataClass?= null){
 
             var slideTarget by remember { mutableStateOf((-400).dp) }
+
+            var kounter by remember { mutableStateOf(0) }
+
+            var klass by remember { mutableStateOf(MyClass()) }
+
+
 
             if(slideState == SlideState.SLIDE_2){
                 slideTarget = (-400).dp
@@ -43,8 +48,52 @@ class Slide3 {
                 .fillMaxHeight()
                 .offset(x = slideAnimation, y = 0.dp)
                 .background(Color(0xFF03A9F4))){
-                Text("Slide #3")
+
+                Column{
+                    Text("Slide #3 - Delegating to another property")
+
+                    Text("A property can delegate its getter and setter to another property. " +
+                            "Such delegation is available for both top-level and class properties " +
+                            "(member and extension).")
+
+                    if(kounter != 0){
+                        Text("delegating oldValue to newValue: ${klass.newValue}", fontWeight = FontWeight.ExtraBold)
+                    }else{
+                        Text("delegating oldValue to newValue: ${klass.newValue}")
+                    }
+
+
+                    Button(onClick = {
+                        if(kounter == 0){
+                            doDelegation(klass, 42)
+                        }
+                        kounter++
+                    }) {
+                        Text("delegate to another property")
+                    }
+
+                    Text("What is the purpose of this?\n\n" +
+                            "This may be useful, for example, when you want to rename a property " +
+                            "in a backward-compatible way: you introduce a new property, annotate " +
+                            "the old one with the @Deprecated annotation, and delegate its implementation.\n\n" +
+                            "Oooooooookay ...... I guess that makes sense")
+                }
+
+
+
             }
         }
+
+        fun doDelegation(klass:MyClass, newValue:Int){
+            // Notification: 'oldName: Int' is deprecated.
+            // Use 'newName' instead
+            klass.oldValue = newValue
+        }
     }
+}
+
+class MyClass {
+    var newValue: Int = 0
+    @Deprecated("Use 'newName' instead", ReplaceWith("newName"))
+    var oldValue: Int by this::newValue
 }
--- app/src/main/java/com/example/cotuit/slides/Slide4.kt
+++ app/src/main/java/com/example/cotuit/slides/Slide4.kt
@@ -4,10 +4,8 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -26,6 +24,10 @@ class Slide4 {
 
             var slideTarget by remember { mutableStateOf((-400).dp) }
 
+            var eh by remember {
+                mutableStateOf(A())
+            }
+
             if(slideState == SlideState.SLIDE_3){
                 slideTarget = (-400).dp
             }
@@ -43,8 +45,122 @@ class Slide4 {
                 .fillMaxHeight()
                 .offset(x = slideAnimation, y = 0.dp)
                 .background(Color(0xFF03A9F4))){
-                Text("Slide #4")
+
+                Column{
+                    Text("Slide #4 - Qualified this")
+                    Text("To denote the current receiver, you use this expressions:\n" +
+                            "\n" +
+                            "In a member of a class, this refers to the current object of that class.\n" +
+                            "\n" +
+                            "In an extension function or a function literal with receiver this denotes the receiver parameter that is passed on the left-hand side of a dot.\n" +
+                            "\n" +
+                            "If this has no qualifiers, it refers to the innermost enclosing scope. To refer to this in other scopes, label qualifiers are used:\n\n" +
+                            "To see this running, you need to review the debug ouput.  There is really nothing to show - it is all about the mechanisms of Kotlin.")
+
+                    Button(onClick = {
+                        var a = A()
+
+                    }) {
+                        Text("Do Qualified *this*")
+                    }
+
+                }
+
+
             }
+
+            var bar = eh.B().z
+        }
+    }
+}
+
+class A { // implicit label @A
+
+    init{
+        println("init A")
+        doC()
+    }
+
+
+    var z = "z"
+
+    fun doC(){
+        println("doC()")
+        var c = C()
+        c.doBar()
+        c.bar
+    }
+
+
+
+
+    inner class B {
+
+        init{
+            println("init B")
+        }
+
+
+        var z = "z"
+        // implicit label @B
+        fun Int.foo() { // implicit label @foo
+            val a = this@A // A's this
+            val b = this@B // B's this
+
+            b.z = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
+            a.z = "zzzzzzzzzzzzzzzzz"
+
+            val c = this // foo()'s receiver, an Int
+            val c1 = this@foo // foo()'s receiver, an Int
+
+            val funLit = lambda@ fun String.() {
+                val d = this // funLit's receiver
+            }
+
+            val funLit2 = { s: String ->
+                // foo()'s receiver, since enclosing lambda expression
+                // doesn't have any receiver
+                val d1 = this
+            }
+
+            val zoop = {
+
+            }
+        }
+    }
+
+    // cannot be accessed ANYWHERE except inside of A()
+    private inner class C{
+
+        var numKraken = 0
+
+        init{
+            println("init C")
+        }
+
+        var z = ""
+        var c:C = this@C
+
+        fun foo(){
+            c.z = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
+        }
+
+        val bar = lambda@ {
+            println("lambda bar")
+        }
+
+        fun doBar(){
+            println("---> doBar")
+            println("---> start Kraken")
+            kraken()
+            kraken = { println("The Kraken was released!") }
+            kraken()
+            println("---> end Kraken")
+        }
+
+        var kraken = {
+            numKraken++
+            println("Release the Kraken!")
         }
     }
 }
