fa340fdfebcfe1d6c1cbcfe54b6ec23c2db0eb0c
m_cor <mikecorbridge@gmail.com>
2021-07-25 10:38:25 -0400

    using compose Modifier similar to CSS

--- app/src/main/java/com/example/cotuit/slides/Slide8.kt
+++ app/src/main/java/com/example/cotuit/slides/Slide8.kt
@@ -1,23 +1,35 @@
 package com.example.cotuit.slides
 
+import android.icu.number.Notation.simple
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.cotuit.test.MyDataClass
 import com.example.cotuit.test.SlideState
+import com.example.cotuit.util.RandNames
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.util.*
 
 class Slide8 {
 
     companion object{
 
+        @RequiresApi(Build.VERSION_CODES.R)
         @Composable
         fun Slide(
             slideState: SlideState,
@@ -25,6 +37,8 @@ class Slide8 {
         ){
 
             var slideTarget by remember { mutableStateOf((-400).dp) }
+            var currentName by remember { mutableStateOf("tbd") }
+            var names = mutableListOf<String>()
 
             if(slideState == SlideState.SLIDE_7){
                 slideTarget = (-400).dp
@@ -44,11 +58,106 @@ class Slide8 {
                 .offset(x = slideAnimation, y = 0.dp)
                 .background(Color(0xFF03A9F4))){
                 Column{
-                    Text("Slide #8")
+
+                    Text("Slide #8 - Kotlin Flow")
                     Text("${dataClass?.name} ${dataClass?.value}")
+
+                    Button(onClick = {
+                        //processValues()
+                        //mainFlow2(names)
+                        mainFlowZ(names){
+                            println("currentName = $it")
+                            currentName = it
+                        }
+                    }) {
+                        Text("Go -> Flow")
+                    }
+
+                    Button(onClick = {
+                        //processValues()
+                        names = RandNames.nameGetter()
+                        currentName = "Names -> Get"
+                    }) {
+                        Text("Names -> Get")
+                    }
+
+                    Text("currentName = $currentName")
                 }
 
             }
         }
-    }
-}
+
+
+
+//        suspend fun getValues(): List<Int> {
+//            delay(1000)
+//            return listOf(1, 2, 3)
+//        }
+
+        fun processValues() {
+            runBlocking {
+                val values = getValues()
+                for (value in values) {
+                    println(value)
+                }
+            }
+        }
+
+        suspend fun getValues(): Sequence<Int> = sequence {
+            Thread.sleep(250)
+            yield(1)
+            Thread.sleep(250)
+            yield(2)
+            Thread.sleep(250)
+            yield(3)
+        }
+
+        val namesFlow = flow {
+            val names = listOf("Jody", "Steve", "Lance", "Joe")
+            for (name in names) {
+                delay(1000)
+                emit(name)
+            }
+        }
+
+        val namesFlowToo = flowOf("Jody", "Steve", "Lance", "Joe")
+
+        fun mainFlow() = runBlocking {
+            namesFlow
+                .map { name -> name.length }
+                .filter { length -> length < 5 }
+                .collect { println(it) }
+
+            println("--------------- finished --------------")
+        }
+
+        fun mainFlow2(names:MutableList<String>) = runBlocking {
+            //val namesFloz = flowOf(names)
+            val namesFloz = flowOf("Jody", "Steve", "Lance", "Joe")
+
+            namesFloz
+                .map { name -> name.uppercase(Locale.getDefault()) }
+                .filter { name -> name.length < 15 }
+                .collect { println(it) }
+
+            println("--------------- finished --------------")
+        }
+
+        @RequiresApi(Build.VERSION_CODES.R)
+        fun mainFlowZ(names:MutableList<String>, callback:(String) -> Unit) = runBlocking<Unit> {
+            // Launch a concurrent coroutine to check if the main thread is blocked
+            launch {
+                names.forEach { name ->
+                    println("current name from list =  $name")
+                    callback(name)
+                    delay(1000)
+                }
+            }
+        }
+
+
+
+
+    } //end companion object
+} // end class
+
--- app/src/main/java/com/example/cotuit/slides/Slide9.kt
+++ app/src/main/java/com/example/cotuit/slides/Slide9.kt
@@ -4,13 +4,18 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cotuit.test.MyDataClass
 import com.example.cotuit.test.SlideState
 
@@ -36,6 +41,25 @@ class Slide9 {
             //}
 
 
+            val modifier0 = Modifier
+                .offset(x=50.dp, y=50.dp).background(color = Color(0xFFCDDC39)).border(width = 2.dp, color = Color.Blue).padding(6.dp)
+            val modifier1 = Modifier
+                .offset(x=25.dp, y=150.dp).background(color = Color(0xFFFFEB3B)).border(width = 4.dp, color = Color.Red).padding(8.dp)
+            val modifier2 = Modifier
+                .offset(x=50.dp, y=200.dp).background(color = Color(0xFFEB8565)).border(width = 6.dp, color = Color.Green).padding(10.dp)
+            val modifier3 = Modifier
+                .offset(x=25.dp, y=250.dp).background(color = Color(0xFFDA4F24)).border(width = 6.dp, color = Color.White).padding(15.dp)
+            val modifier4 = Modifier
+                .offset(x=50.dp, y=300.dp).background(color = Color(0xFF00BCD4)).border(width = 6.dp, color = Color.Black).padding(20.dp)
+
+            val modifier5 = modifier0.fillMaxSize()
+
+
+            var whichModifier by remember { mutableStateOf(modifier0)}
+            var ndx by remember {
+                mutableStateOf(0)
+            }
+
             val slideAnimation: Dp by animateDpAsState(
                 targetValue = if (slideState == SlideState.SLIDE_9) 0.dp else slideTarget,
                 animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = 100f),
@@ -46,11 +70,41 @@ class Slide9 {
                 .fillMaxHeight()
                 .offset(x = slideAnimation, y = 0.dp)
                 .background(Color(0xFF03A9F4))){
-                Column{
-                    Text("Slide #9")
-                    Text("${dataClass?.name} ${dataClass?.value}")
+
+                Text("Are Modifiers similar to CSS?", fontSize = 28.sp, modifier = Modifier.background(Color(0xFFE7CE81)))
+
+                ViewColumn(whichModifier, dataClass){
+                    ndx++
+                    when(ndx){
+                        1 -> whichModifier = modifier1
+                        2 -> whichModifier = modifier2
+                        3 -> whichModifier = modifier3
+                        4 -> whichModifier = modifier4
+                        5 -> whichModifier = modifier5
+                        else -> {
+                            ndx = 0
+                            whichModifier = modifier0
+                        }
+                    }
                 }
 
+                if(ndx == 5){
+                    Text("YES THEY ARE!", fontSize = 28.sp, color = Color.White, fontWeight = FontWeight.ExtraBold,
+                        modifier = Modifier.offset(x=50.dp, y=450.dp))
+                }
+
+
+            }
+        }
+
+        @Composable
+        fun ViewColumn(modifier: Modifier, dataClass: MyDataClass?, callback:() -> Unit){
+            Column (modifier = modifier){
+                Text("Slide #9 - dynamically loaded modifiers")
+                Text("${dataClass?.name} ${dataClass?.value}")
+                Button(onClick = { callback() }) {
+                    Text("switch Modifier?")
+                }
             }
         }
     }
